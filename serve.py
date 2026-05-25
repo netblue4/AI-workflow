77c557f97f195d44811bc0a093a81dde6a881703
netblue4 <netblue4@googlemail.com>
2026-05-25 11:59:57 +0200

    Delete serve.py

--- serve.py
+++
@@ -1,3 +0,0 @@
-import http.server, os
-os.chdir('/Users/michaelbergman/Documents/GitHub/AI-workflow')
-http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler, port=3737, bind='127.0.0.1')
